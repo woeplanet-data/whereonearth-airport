8abe97e0e1dcb7c86c3f0d3732b9a2d2f6109b6b
straup <straup@gmail.com>
2011-09-24 19:26:59 +0200

    generate meta files

--- bin/generate_meta.py
+++ bin/generate_meta.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 
-import pprint
-
+import sys
 import json
 import os
+import csv
+
+import utils
+
+import logging
+logging.basicConfig(level=logging.INFO)
 
 countries = {}
 iso_codes = {}
@@ -19,11 +24,11 @@ if __name__ == '__main__':
         for f in files:
 
             path = os.path.join(root, f)
-
             fh = open(path)
             data = json.load(fh)
 
-            props = data['features'][0]['properties']
+            feature = data['features'][0]
+            props = feature['properties']
 
             this_woeid = props['woe:id']
             this_country = None
@@ -41,12 +46,84 @@ if __name__ == '__main__':
             if not countries.get(this_country, False):
                 countries[this_country] = []
 
-            countries[this_country].append(this_woeid)
+            try:
+                centroid = [ props['longitude'], props['latitude'] ]
+            except Exception, e:
+                logging.error("failed to process %s: %s" % (path, e))
+                # wtf?
+                continue
+
+            woeid = props['woe:id']
+
+            root = utils.woeid2path(woeid)
+            path = "/%s/%s.json" % (root, woeid)
+
+            short = {
+                'geometry' : { 'type': 'Point', 'coordinates': centroid },
+                'id': feature['id'],
+                'properties' : {
+                    'name': props['name'],
+                    'woe:id': woeid,
+                    'href': path
+                    }
+                }
+
+            for code in ('iata:code', 'icao:code'):
+                if props.get(code, False):
+                    short['properties'][code] = props[code]
+
+            countries[this_country].append(short)
 
             if not iso_codes.get(this_country, False):
                 iso_codes[this_country] = props['iso']
 
-for woeid, ports in countries.items():
-    print "%s: %s" % (iso_codes[woeid], len(ports))
+# generate some basic stats (as a CVS file)
 
-# print pprint.pformat(countries)
+# FIX ME
+
+fh = open('../meta/countries.csv', 'w')
+
+writer = csv.writer(fh)
+writer.writerow(('iso', 'woeid', 'airports'))
+
+tmp = {}
+
+for k,v in iso_codes.items():
+    tmp[v] = k
+
+codes = tmp.keys()
+codes.sort()
+
+for iso in codes:
+
+    woeid = tmp[iso]
+    ports = countries[woeid]
+
+    logging.info("%s (%s) : %s airports" % (iso, woeid, len(ports)))
+    writer.writerow((iso, woeid, len(ports)))
+
+fh.close()
+
+# Now generate files for each country
+
+for woeid, features in countries.items():
+
+    iso = iso_codes[woeid]
+
+    collection = {
+        'type': 'FeatureCollection',
+        'features': features,
+        'properties': {
+            'woe:id': woeid,
+            'iso': iso
+            }
+        }
+
+    # FIX ME
+
+    path = "../meta/%s.json" % iso
+    logging.info("write %s" % path)
+
+    fh = open(path, 'w')
+    utils.write_json(collection, fh)
+    fh.close()
---
+++ bin/utils.py
@@ -0,0 +1,36 @@
+import json
+import re
+
+def write_json(data, out, indent=2): 
+
+    encoded = json.JSONEncoder(indent=indent)
+    encoded = encoded.iterencode(data)
+
+    float_pat = re.compile(r'^-?\d+\.\d+$')
+
+    for atom in encoded:
+        if float_pat.match(atom):
+            out.write('%.3f' % float(atom))
+        else:
+            out.write(atom)
+
+def woeid2path(id):
+
+    tmp = str(id)
+    parts = []
+
+    while len(tmp) > 3:
+        parts.append(tmp[0:3])
+        tmp = tmp[3:]
+
+    if len(tmp):
+        parts.append(tmp)
+
+    return "/".join(parts)
+
+def scrub_placetype(type):
+    type = type.lower()
+    type = type.replace(" ", "-")
+    return type
+
+
